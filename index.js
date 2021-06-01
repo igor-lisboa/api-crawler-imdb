971105168fcb6b38877b0a83f69e77080a8f2203
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-05-31 23:39:24 -0300

    Including express dependency

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get(`/`, async (req, res) => {
+    return res.json({ itIsWorking: "uhul" });
+});
+
+app.listen(3000, () => {
+    console.log(`The application is listening on port 3000!`);
+});
