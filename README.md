d5c3b3e23806e89e0d2460d5301c56c5f6ba0895
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-06-01 01:01:47 -0300

    Getting all data and preparing filters

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # api-crawler-imdb
 API to get data from IMDB website
+
+## Problem Statement
+Build search for the top 1000 IMDB movies. Users should be able to search for different aspects of the movie (e.g. director name) and get back the set of movies related to it. For instance, in our implementation:
+
+Searching for “spielberg” returned the following list:
+
+```javascript
+[ "Schindler's List",
+"Saving Private Ryan",
+"Raiders of the Lost Ark",
+"Indiana Jones and the Last Crusade",
+"Jurassic Park",
+"Catch Me If You Can",
+"Jaws",
+"E.T. the ExtraTerrestrial",
+"Empire of the Sun",
+"The Color Purple",
+"Minority Report",
+"Close Encounters of the Third Kind",
+"Bridge of Spies",
+"Indiana Jones and the Temple of Doom",
+"Munich"]
+```
+Searching for "spielberg hanks" returns:
+
+```javascript
+[ "Saving Private Ryan",
+"Catch Me If You Can",
+"Bridge of Spies"]
+```
+
+This is the list of all movies associated with both Spielberg and Hanks.
+
+## Components
+
+You can use the language of your choice.
+
+1. Crawl: You will need to crawl the IMDB listing pages (there are multiple pages) and all the movies linked off of the listing pages.
+2. Parse: You will need to parse the pages to extract the right information.
+3. Search database: “Index” the information in some way. Given the scale of the problem is not large, you can use a simple in-memory data structure. Please implement it, rather than leveraging external tools for this purpose.
+4. Expose a simple API that returns the movie names given a search term.
+
+## Non Goals
+
+This is a very open-ended problem. The goal is not to build a comprehensive solution. Feel free to take decisions on the scope of what you want to accomplish. You can take as much or as little time on the exercise but my recommendation would be spending 2-3 hours at the most.
+
+Some decision you can make on the scope:
+
+* Relevance and ranking are not in scope.
+* Your data structure should be optimized for lookup time (take into account the number of lookups). But you don't have to worry about memory scale at this point.
+* There is no UX needed. A simple API would do.
+* Feel free to make your own decisions on the scope.
+
+## Helpful tips
+
+* Most languages provide libraries for extracting information from web pages using CSS selectors (e.g BeautifulSoupfor python)
+* Chrome provides you the ability to look at the CSS selector for a given element on the page
+* Important: Have Fun!!
+
+## Deliverables
+
+* Code for this exercise along with instructions on how to run it (if possible, check in the code to github with a README).
+* Thoughts on some simplifying assumptions you made and why.
+* Some thoughts on If you had more time, how you would improve the solution from the perspective of
+software architecture, scale, performance or quality.
+
+Please send any questions/clarification and the solution to us
--- index.ts
+++ index.ts
@@ -4,37 +4,99 @@ import axios, { AxiosResponse } from 'axios';
 import cheerio, { CheerioAPI } from 'cheerio';
 import NodeCache from 'node-cache';
 
+const myCache = new NodeCache();
+
 // load dotenv variables
 dotenv.config();
-
-const myCache = new NodeCache();
 
 const app = express();
 
 const port: string = process.env.PORT || `3000`;
 const cacheMinutes: string = process.env.CACHE_MINUTES || `5`;
 
+
 app.get(`/`, async (req, res) => {
-    console.log(myCache.keys());
-
+    const searchQuery: string = req.query.q?.toString() || `all`;
     const cacheSeconds: number = parseInt(req.query.cacheMinutes?.toString() || cacheMinutes) * 60;
+    const notPluckReturn: boolean = req.query.pluck?.toString() === `0`;
+    const notUseCache: boolean = req.query.cache?.toString() === `0`;
 
-    const baseUrl: string = `https://www.imdb.com`;
+    let titles: { title: string; summary: string; directors: string[]; starActors: string[]; releasedYear: number; position: number; stars: number; metaScore: number; certificate: string; runtimeMinutes: number; genres: string[]; votes: number; gross: string; }[] = [];
 
-    let urlNextPage: string | undefined = `/search/title/?groups=top_1000`;
+    // if the cache query param is not 0 and myCache has the searchQuery => populate titles with the cachedTitles ELSE search on imdb
+    if (notUseCache === false && myCache.has(searchQuery)) {
+        const cachedTitles: { title: string; summary: string; directors: string[]; starActors: string[]; releasedYear: number; position: number; stars: number; metaScore: number; certificate: string; runtimeMinutes: number; genres: string[]; votes: number; gross: string; }[] | undefined = myCache.get(searchQuery);
+        if (cachedTitles !== undefined) {
+            titles = cachedTitles;
+        }
+    } else {
+        const baseUrl: string = `https://www.imdb.com`;
 
-    if (!myCache.has("test")) {
+        let urlNextPage: string | undefined = `/search/title/?groups=top_1000`;
+
         // walk through pages
         while (urlNextPage !== undefined) {
             const response: AxiosResponse<any> = await axios.get(`${baseUrl}${urlNextPage}`);
             const $: CheerioAPI = cheerio.load(response.data);
             urlNextPage = $(`.lister-page-next.next-page`).first().attr(`href`);
+
+            $(`.lister-item.mode-advanced`).each((i, element) => {
+                const cheerioElement = $(element);
+
+                const itemContent = cheerioElement.find(`.lister-item-content`).first();
+                const ratingsBar = itemContent.find(`.ratings-bar`).first();
+                const h3Title = itemContent.find(`h3.lister-item-header`).first();
+
+                const title: string = h3Title.find(`a`).first().text().trim();
+                const summary: string = itemContent.find(`p:nth-child(4)`).text().trim();
+                const releasedYear: number = parseInt(h3Title.find(`.lister-item-year.text-muted.unbold`).text().trim().replace(/\D/g, ``));
+                const position: number = parseInt(h3Title.find(`.lister-item-index.unbold.text-primary`).text().trim().replace(/\D/g, ``));
+                const stars: number = parseInt(ratingsBar.find(`.inline-block.ratings-imdb-rating>strong`).first().text().trim().replace(/\D/g, ``)) / 10;
+                const metaScore: number = parseInt(ratingsBar.find(`.inline-block.ratings-metascore>span.metascore`).first().text().trim().replace(/\D/g, ``));
+
+                const certificate: string = itemContent.find(`p:nth-child(2) > span.certificate`).text().trim();
+                const runtimeMinutes: number = parseInt(itemContent.find(`p:nth-child(2) > span.runtime`).text().trim().replace(` min`, ``));
+                const genres: string[] = itemContent.find(`p:nth-child(2) > span.genre`).text().trim().split(`, `);
+
+                const infoDirectorsAndStars: string[] = itemContent.find(`p:nth-child(5)`).text().trim().replace(`  `, ``).split(`Stars:`);
+                const directors: string[] = infoDirectorsAndStars[0].replace(`Director:\n`, ``).replace(`Directors:\n`, ``).replace(`, \n`, `, `).replace(`\n               | \n    `, ``).split(`, `);
+                const starActors: string[] = infoDirectorsAndStars[1].replace(/(\r\n|\n|\r)/gm, ``).split(`, `);
+
+                const votes: number = parseInt(itemContent.find(`p.sort-num_votes-visible > span:nth-child(2)`).text().trim().replace(`,`, ``));
+                const gross: string = itemContent.find(`p.sort-num_votes-visible > span:nth-child(5)`).text().trim();
+
+
+                let searchedText: string = searchQuery.toLowerCase();
+                if (searchedText === `all`) {
+                    searchedText = ``;
+                }
+
+                let includeTitle: boolean = false;
+
+                // splitting the searchedText to search each part separate
+                searchedText.split(` `).forEach(itemToSearch => {
+                    // check if title or directors or starActors or genres includes the searched text, if includes, push to titles
+                    if (title.toLowerCase().includes(itemToSearch) || directors.join(`,`).toLowerCase().includes(itemToSearch) || starActors.join(`,`).toLowerCase().includes(itemToSearch) || genres.join(`,`).toLowerCase().includes(itemToSearch)) {
+                        includeTitle = true;
+                    }
+                });
+
+                if (includeTitle) {
+                    titles.push({ title, summary, directors, starActors, releasedYear, position, stars, metaScore, certificate, runtimeMinutes, genres, votes, gross });
+                }
+
+            });
         }
 
-        myCache.set("test", { cache: "test" }, cacheSeconds);
+        myCache.set(searchQuery, titles, cacheSeconds);
     }
 
-    return res.json(myCache.get("test"));
+
+    if (notPluckReturn === true) {
+        return res.json(titles);
+    }
+
+    return res.json(titles.map(title => title.title));
 });
 
 app.listen(port, () => {
