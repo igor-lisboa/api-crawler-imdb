5598c16752fc9375762187f379c68d2669df6477
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-06-01 00:25:49 -0300

    Enabling the use of cache

--- index.ts
+++ index.ts
@@ -2,29 +2,39 @@ import express from 'express';
 import dotenv from 'dotenv';
 import axios, { AxiosResponse } from 'axios';
 import cheerio, { CheerioAPI } from 'cheerio';
+import NodeCache from 'node-cache';
 
 // load dotenv variables
 dotenv.config();
 
+const myCache = new NodeCache();
+
 const app = express();
 
 const port: string = process.env.PORT || `3000`;
+const cacheMinutes: string = process.env.CACHE_MINUTES || `5`;
 
 app.get(`/`, async (req, res) => {
+    console.log(myCache.keys());
+
+    const cacheSeconds: number = parseInt(req.query.cacheMinutes?.toString() || cacheMinutes) * 60;
+
     const baseUrl: string = `https://www.imdb.com`;
 
     let urlNextPage: string | undefined = `/search/title/?groups=top_1000`;
 
-    // walk through pages
-    while (urlNextPage !== undefined) {
-        const response: AxiosResponse<any> = await axios.get(`${baseUrl}${urlNextPage}`);
-        const $: CheerioAPI = cheerio.load(response.data);
-        urlNextPage = $(`.lister-page-next.next-page`).first().attr(`href`);
-        console.log(urlNextPage);
+    if (!myCache.has("test")) {
+        // walk through pages
+        while (urlNextPage !== undefined) {
+            const response: AxiosResponse<any> = await axios.get(`${baseUrl}${urlNextPage}`);
+            const $: CheerioAPI = cheerio.load(response.data);
+            urlNextPage = $(`.lister-page-next.next-page`).first().attr(`href`);
+        }
+
+        myCache.set("test", { cache: "test" }, cacheSeconds);
     }
 
-
-    return res.json({ itIsWorking: "uhul" });
+    return res.json(myCache.get("test"));
 });
 
 app.listen(port, () => {
