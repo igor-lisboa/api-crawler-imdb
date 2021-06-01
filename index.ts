c821be4d6c4290c0c507e31ec1b2c001823e1d9e
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-06-01 00:07:56 -0300

    Geting the imdb page using axios

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 // load dotenv variables
 dotenv.config();
@@ -9,6 +10,13 @@ const app = express();
 const port: string = process.env.PORT || `3000`;
 
 app.get(`/`, async (req, res) => {
+    const baseUrl: string = `https://www.imdb.com`;
+
+    let urlNextPage: string | undefined = `/search/title/?groups=top_1000`;
+
+    const response = await axios.get(`${baseUrl}${urlNextPage}`);
+    console.log(response.data);
+
     return res.json({ itIsWorking: "uhul" });
 });
 
