4e2dd093a3c556c4f2e1a99e6f65fa4faa8ddac6
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-05-31 23:54:17 -0300

    Including dotenv loading

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+// load dotenv variables
+dotenv.config();
+
 const app = express();
+
+const port: string = process.env.PORT || `3000`;
 
 app.get(`/`, async (req, res) => {
     return res.json({ itIsWorking: "uhul" });
 });
 
-app.listen(3000, () => {
-    console.log(`The application is listening on port 3000!`);
+app.listen(port, () => {
+    console.log(`The application is listening on port ${port}!`);
 });
